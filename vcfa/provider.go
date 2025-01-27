a4eb7d8d53f3442d28d1b242a5816512c90f8f54
Dainius <Didainius@users.noreply.github.com>
2025-01-27 17:55:07 +0200

    Add Org networking settings vcfa_org_networking resource + data source (#15)

--- vcfa/provider.go
+++ vcfa/provider.go
@@ -51,6 +51,7 @@ var globalDataSourceMap = map[string]*schema.Resource{
 	"vcfa_edge_cluster":          datasourceVcfaEdgeCluster(),         // 1.0
 	"vcfa_edge_cluster_qos":      datasourceVcfaEdgeClusterQos(),      // 1.0
 	"vcfa_org_oidc":              datasourceVcfaOrgOidc(),             // 1.0
+	"vcfa_org_networking":        datasourceVcfaOrgNetworking(),       // 1.0
 }
 
 var globalResourceMap = map[string]*schema.Resource{
@@ -65,6 +66,7 @@ var globalResourceMap = map[string]*schema.Resource{
 	"vcfa_provider_gateway":     resourceVcfaProviderGateway(),    // 1.0
 	"vcfa_edge_cluster_qos":     resourceVcfaEdgeClusterQos(),     // 1.0
 	"vcfa_org_oidc":             resourceVcfaOrgOidc(),            // 1.0
+	"vcfa_org_networking":       resourceVcfaOrgNetworking(),      // 1.0
 }
 
 // Provider returns a terraform.ResourceProvider.
---
+++ vcfa/resource_vcfa_org_networking_test.go
@@ -0,0 +1,129 @@
+//go:build org || tm || ALL || functional
+
+package vcfa
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccVcfaOrgNetworking(t *testing.T) {
+	preTestChecks(t)
+	skipIfNotSysAdmin(t)
+
+	var params = StringMap{
+		"Testname": t.Name(),
+		"Tags":     "tm org",
+	}
+	testParamsNotEmpty(t, params)
+
+	configText1 := templateFill(testAccVcfaOrgNetworkingStep1, params)
+	params["FuncName"] = t.Name() + "-step2"
+	configText2 := templateFill(testAccVcfaOrgNetworkingStep2, params)
+	params["FuncName"] = t.Name() + "-step3"
+	configText3 := templateFill(testAccVcfaOrgNetworkingStep3, params)
+	params["FuncName"] = t.Name() + "-step4"
+	configText4 := templateFill(testAccVcfaOrgNetworkingStep4DS, params)
+
+	debugPrintf("#[DEBUG] CONFIGURATION step1: %s\n", configText1)
+	debugPrintf("#[DEBUG] CONFIGURATION step2: %s\n", configText2)
+	debugPrintf("#[DEBUG] CONFIGURATION step3: %s\n", configText3)
+	debugPrintf("#[DEBUG] CONFIGURATION step4: %s\n", configText4)
+	if vcfaShortTest {
+		t.Skip(acceptanceTestsSkipped)
+		return
+	}
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: configText1,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("vcfa_org.test", "name", t.Name()),
+					resource.TestCheckResourceAttrPair("vcfa_org.test", "id", "vcfa_org_networking.test", "id"),
+					resource.TestCheckResourceAttr("vcfa_org_networking.test", "log_name", "l-one"),
+				),
+			},
+			{
+				Config: configText2,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("vcfa_org.test", "name", t.Name()),
+					resource.TestCheckResourceAttrPair("vcfa_org.test", "id", "vcfa_org_networking.test", "id"),
+					resource.TestCheckResourceAttr("vcfa_org_networking.test", "log_name", ""),
+				),
+			},
+			{
+				Config: configText3,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("vcfa_org.test", "name", t.Name()+""),
+					resource.TestCheckResourceAttrPair("vcfa_org.test", "id", "vcfa_org_networking.test", "id"),
+					resource.TestCheckResourceAttr("vcfa_org_networking.test", "log_name", "l-one-u"),
+				),
+			},
+			{
+				ResourceName:      "vcfa_org_networking.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateId:     params["Testname"].(string), // Org name
+			},
+			{
+				Config: configText4,
+				Check: resource.ComposeTestCheckFunc(
+					resourceFieldsEqual("vcfa_org_networking.test", "data.vcfa_org_networking.test", nil),
+				),
+			},
+		},
+	})
+
+	postTestChecks(t)
+}
+
+const testAccVcfaOrgNetworkingStep1 = `
+resource "vcfa_org" "test" {
+  name         = "{{.Testname}}"
+  display_name = "terraform-test"
+  description  = "terraform test"
+  is_enabled   = true
+}
+
+resource "vcfa_org_networking" "test" {
+  org_id   = vcfa_org.test.id
+  log_name = "l-one"
+}
+`
+
+const testAccVcfaOrgNetworkingStep2 = `
+resource "vcfa_org" "test" {
+  name         = "{{.Testname}}"
+  display_name = "terraform-test"
+  description  = ""
+  is_enabled   = true
+}
+
+resource "vcfa_org_networking" "test" {
+  org_id   = vcfa_org.test.id
+  log_name = ""
+}
+`
+
+const testAccVcfaOrgNetworkingStep3 = `
+resource "vcfa_org" "test" {
+  name         = "{{.Testname}}"
+  display_name = "terraform-test"
+  description  = ""
+  is_enabled   = true
+}
+
+resource "vcfa_org_networking" "test" {
+  org_id   = vcfa_org.test.id
+  log_name = "l-one-u"
+}
+`
+
+const testAccVcfaOrgNetworkingStep4DS = testAccVcfaOrgNetworkingStep3 + `
+data "vcfa_org_networking" "test" {
+  org_id = vcfa_org.test.id
+}
+`
