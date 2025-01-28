e789481ae6d7fe5883d8c153db679fc131471351
Dainius <Didainius@users.noreply.github.com>
2025-01-28 14:38:24 +0200

    WIP
    Signed-off-by: Dainius <Didainius@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -67,4 +67,5 @@ require (
 	sigs.k8s.io/yaml v1.4.0 // indirect
 )
 
-replace github.com/vmware/go-vcloud-director/v3 => github.com/Didainius/go-vcloud-director/v3 v3.0.0-alpha.4.0.20250128090842-1cb68a82b51a
+//replace github.com/vmware/go-vcloud-director/v3 => github.com/Didainius/go-vcloud-director/v3 v2.12.1-0.20211018060826-c7f8ab32330e
+replace github.com/vmware/go-vcloud-director/v3 => ../go-vcloud-director
---
+++ vcfa/resource_vcfa_org_regional_networking_vpc_qos.go
@@ -0,0 +1,259 @@
+package vcfa
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/vmware/go-vcloud-director/v3/govcd"
+	"github.com/vmware/go-vcloud-director/v3/types/v56"
+)
+
+const labelVcfaOrgRegionalNetworkingVpcQos = "Regional Networking VPC Connectivity Profile QoS"
+
+func resourceVcfaOrgRegionalNetworkingVpcQos() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceVcfaOrgRegionalNetworkingVpcQosCreateUpdate,
+		ReadContext:   resourceVcfaOrgRegionalNetworkingVpcQosRead,
+		UpdateContext: resourceVcfaOrgRegionalNetworkingVpcQosCreateUpdate,
+		DeleteContext: resourceVcfaOrgRegionalNetworkingVpcQosDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceVcfaOrgRegionalNetworkingImport, // Org Regional Network Settings, because VPC QoS is a property of it
+		},
+
+		Schema: map[string]*schema.Schema{
+			"org_regional_networking_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: fmt.Sprintf("ID of %s", labelVcfaRegionalNetworkingSetting),
+			},
+			"edge_cluster_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: fmt.Sprintf("ID of parent %s", labelVcfaEdgeCluster),
+			},
+			"ingress_committed_bandwidth_mbps": {
+				Type:        schema.TypeString, // string + validation due to usual problem of differentiation between 0 and empty value for TypeInt
+				Optional:    true,
+				Computed:    true,
+				Description: fmt.Sprintf("Ingress committed bandwidth in Mbps for %s", labelVcfaEdgeCluster),
+				ValidateDiagFunc: validation.AnyDiag(
+					validation.ToDiagFunc(validation.StringIsEmpty),
+					IsIntAndAtLeast(-1), // -1 is unlimited
+				),
+				RequiredWith: []string{"ingress_burst_size_bytes"},
+			},
+			"ingress_burst_size_bytes": {
+				Type:        schema.TypeString, // string + validation due to usual problem of differentiation between 0 and empty value for TypeInt
+				Optional:    true,
+				Computed:    true,
+				Description: fmt.Sprintf("Ingress burst size bytes for %s", labelVcfaEdgeCluster),
+				ValidateDiagFunc: validation.AnyDiag(
+					validation.ToDiagFunc(validation.StringIsEmpty),
+					IsIntAndAtLeast(-1), // -1 is unlimited
+				),
+				RequiredWith: []string{"ingress_committed_bandwidth_mbps"},
+			},
+			"egress_committed_bandwidth_mbps": {
+				Type:        schema.TypeString, // string + validation due to usual problem of differentiation between 0 and empty value for TypeInt
+				Optional:    true,
+				Computed:    true,
+				Description: fmt.Sprintf("Egress committed bandwidth in Mbps for %s", labelVcfaEdgeCluster),
+				ValidateDiagFunc: validation.AnyDiag(
+					validation.ToDiagFunc(validation.StringIsEmpty),
+					IsIntAndAtLeast(-1), // -1 is unlimited
+				),
+				RequiredWith: []string{"egress_burst_size_bytes"},
+			},
+			"egress_burst_size_bytes": {
+				Type:        schema.TypeString, // string + validation due to usual problem of differentiation between 0 and empty value for TypeInt
+				Optional:    true,
+				Computed:    true,
+				Description: fmt.Sprintf("Ingress burst size bytes for %s", labelVcfaEdgeCluster),
+				ValidateDiagFunc: validation.AnyDiag(
+					validation.ToDiagFunc(validation.StringIsEmpty),
+					IsIntAndAtLeast(-1), // -1 is unlimited
+				),
+				RequiredWith: []string{"egress_committed_bandwidth_mbps"},
+			},
+		},
+	}
+}
+
+func resourceVcfaOrgRegionalNetworkingVpcQosCreateUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	vcfaClient := meta.(*VCDClient)
+	rns, err := vcfaClient.GetTmRegionalNetworkingSettingById(d.Get("org_regional_networking_id").(string))
+	if err != nil {
+		return diag.Errorf("error looking up %s by ID: %s", labelVcfaOrgNetworking, err)
+	}
+
+	// ID is Org Regional Networking Setting ID
+	d.SetId(rns.TmRegionalNetworkingSetting.ID)
+
+	cfg, err := getTmOrgRegionalNetworkingVpcQosType(vcfaClient, rns, d, true)
+	if err != nil {
+		return diag.Errorf("error getting %s configuration: %s", labelVcfaOrgRegionalNetworkingVpcQos, err)
+	}
+
+	_, err = rns.UpdateDefaultVpcConnectivityProfile(cfg)
+	if err != nil {
+		return diag.Errorf("error setting %s configuration: %s", labelVcfaOrgRegionalNetworkingVpcQos, err)
+	}
+
+	return resourceVcfaOrgRegionalNetworkingVpcQosRead(ctx, d, meta)
+}
+
+func resourceVcfaOrgRegionalNetworkingVpcQosRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	vcfaClient := meta.(*VCDClient)
+	rns, err := vcfaClient.GetTmRegionalNetworkingSettingById(d.Get("org_regional_networking_id").(string))
+	if err != nil {
+		if govcd.ContainsNotFound(err) {
+			d.SetId("")
+			return nil
+		}
+		return diag.Errorf("error looking up %s by ID: %s", labelVcfaOrgNetworking, err)
+	}
+
+	// ID is Org Regional Networking Setting ID
+	d.SetId(rns.TmRegionalNetworkingSetting.ID)
+
+	// fetch VPC profile
+	vpcProfile, err := rns.GetDefaultVpcConnectivityProfile()
+	if err != nil {
+		return diag.Errorf("error retrieving %s: %s", labelVcfaOrgRegionalNetworkingVpcQos, err)
+	}
+
+	err = setTmOrgRegionalNetworkingVpcQosData(vcfaClient, d, vpcProfile)
+	if err != nil {
+		return diag.Errorf("error storing %s configuration to state: %s", labelVcfaOrgRegionalNetworkingVpcQos, err)
+	}
+
+	return nil
+}
+
+func resourceVcfaOrgRegionalNetworkingVpcQosDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	vcfaClient := meta.(*VCDClient)
+	rns, err := vcfaClient.GetTmRegionalNetworkingSettingById(d.Get("org_regional_networking_id").(string))
+	if err != nil {
+		return diag.Errorf("error looking up %s by ID: %s", labelVcfaOrgNetworking, err)
+	}
+
+	// ID is Org Regional Networking Setting ID
+	d.SetId(rns.TmRegionalNetworkingSetting.ID)
+
+	cfg, err := getTmOrgRegionalNetworkingVpcQosType(vcfaClient, rns, d, false)
+	if err != nil {
+		return diag.Errorf("error getting %s configuration: %s", labelVcfaOrgRegionalNetworkingVpcQos, err)
+	}
+
+	_, err = rns.UpdateDefaultVpcConnectivityProfile(cfg)
+	if err != nil {
+		return diag.Errorf("error removing custom %s configuration: %s", labelVcfaOrgRegionalNetworkingVpcQos, err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func getTmOrgRegionalNetworkingVpcQosType(vcfaClient *VCDClient, rns *govcd.TmRegionalNetworkingSetting, d *schema.ResourceData, isCreatedUpdate bool) (*types.TmRegionalNetworkingVpcConnectivityProfile, error) {
+	// The QoS config of Edge Cluster that is used for Region is used as main configuration. One
+	// can override it per Org Regional Networking configuration
+
+	// Starting with parent Edge Cluster QoS configuration
+	existingVpcProfile, err := rns.GetDefaultVpcConnectivityProfile()
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving %s: %s", labelVcfaOrgRegionalNetworkingVpcQos, err)
+	}
+
+	// Fetch edge cluster
+	if existingVpcProfile.ServiceEdgeClusterRef == nil {
+		return nil, fmt.Errorf("could not find %s for %s with ID %s", labelVcfaEdgeCluster, labelVcfaOrgNetworking, rns.TmRegionalNetworkingSetting.ID)
+	}
+
+	// Fetch parent Edge Cluster QoS Config
+	backingEdgeCluster, err := vcfaClient.GetTmEdgeClusterById(existingVpcProfile.ServiceEdgeClusterRef.ID)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving parent %s for %s with ID %s", labelVcfaEdgeCluster, labelVcfaOrgNetworking, rns.TmRegionalNetworkingSetting.ID)
+	}
+
+	// Starting with QoS Config that is based on parent Edge Cluster
+	newQosConfig := &types.VpcConnectivityProfileQosConfig{
+		IngressProfile: &types.VpcConnectivityProfileQosProfile{
+			Type:                   "DEFAULT",
+			CommittedBandwidthMbps: backingEdgeCluster.TmEdgeCluster.DefaultQosConfig.IngressProfile.CommittedBandwidthMbps,
+			BurstSizeBytes:         backingEdgeCluster.TmEdgeCluster.DefaultQosConfig.IngressProfile.BurstSizeBytes,
+		},
+		EgressProfile: &types.VpcConnectivityProfileQosProfile{
+			Type:                   "DEFAULT",
+			CommittedBandwidthMbps: backingEdgeCluster.TmEdgeCluster.DefaultQosConfig.EgressProfile.CommittedBandwidthMbps,
+			BurstSizeBytes:         backingEdgeCluster.TmEdgeCluster.DefaultQosConfig.EgressProfile.BurstSizeBytes,
+		},
+	}
+
+	// overriding fields to specified ones if it is Create or Update. Not changing any values for Delete
+	if isCreatedUpdate {
+		// Overriding just provided fields
+		ingressCommittedBandwidthMbps := d.Get("ingress_committed_bandwidth_mbps").(string)
+		ingressBurstSizeBytes := d.Get("ingress_burst_size_bytes").(string)
+
+		if ingressCommittedBandwidthMbps != "" || ingressBurstSizeBytes != "" { // schema requires both to be set if one is set
+			newQosConfig.IngressProfile = &types.VpcConnectivityProfileQosProfile{
+				Type:                   "CUSTOM",
+				CommittedBandwidthMbps: mustStrToInt(ingressCommittedBandwidthMbps), // schema validates that fields are ints
+				BurstSizeBytes:         mustStrToInt(ingressBurstSizeBytes),
+			}
+		}
+
+		egressCommittedBandwidthMbps := d.Get("egress_committed_bandwidth_mbps").(string)
+		egressBurstSizeBytes := d.Get("egress_burst_size_bytes").(string)
+		if egressCommittedBandwidthMbps != "" || egressBurstSizeBytes != "" { // schema requires both to be set if one is set
+			newQosConfig.EgressProfile = &types.VpcConnectivityProfileQosProfile{
+				Type:                   "CUSTOM",
+				CommittedBandwidthMbps: mustStrToInt(egressCommittedBandwidthMbps), // schema validates that fields are ints
+				BurstSizeBytes:         mustStrToInt(egressBurstSizeBytes),
+			}
+		}
+	}
+
+	// update existing VPC profile with new QoS config, leave other body as it was
+	existingVpcProfile.QosConfig = newQosConfig
+
+	return existingVpcProfile, nil
+}
+
+func setTmOrgRegionalNetworkingVpcQosData(_ *VCDClient, d *schema.ResourceData, t *types.TmRegionalNetworkingVpcConnectivityProfile) error {
+	if t == nil {
+		return fmt.Errorf("empty %s received", labelVcfaOrgRegionalNetworkingVpcQos)
+	}
+
+	if t.ServiceEdgeClusterRef != nil {
+		dSet(d, "edge_cluster_id", t.ServiceEdgeClusterRef.ID)
+	}
+
+	dSet(d, "ingress_committed_bandwidth_mbps", nil)
+	dSet(d, "ingress_burst_size_bytes", nil)
+	if t.QosConfig.IngressProfile != nil {
+		strValue := strconv.Itoa(t.QosConfig.IngressProfile.BurstSizeBytes)
+		dSet(d, "ingress_burst_size_bytes", strValue)
+
+		strValueCommitted := strconv.Itoa(t.QosConfig.IngressProfile.CommittedBandwidthMbps)
+		dSet(d, "ingress_committed_bandwidth_mbps", strValueCommitted)
+	}
+
+	dSet(d, "egress_committed_bandwidth_mbps", nil)
+	dSet(d, "egress_burst_size_bytes", nil)
+	if t.QosConfig.EgressProfile != nil {
+		strValueCommitted := strconv.Itoa(t.QosConfig.EgressProfile.CommittedBandwidthMbps)
+		dSet(d, "egress_committed_bandwidth_mbps", strValueCommitted)
+
+		strValueBurstSize := strconv.Itoa(t.QosConfig.EgressProfile.BurstSizeBytes)
+		dSet(d, "egress_burst_size_bytes", strValueBurstSize)
+	}
+
+	return nil
+}
