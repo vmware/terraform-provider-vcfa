143840e823611f33fd8f3edc33a393fa7d7979c8
Dainius <Didainius@users.noreply.github.com>
2025-03-21 12:31:12 +0200

    Simplify code
    Signed-off-by: Dainius <Didainius@users.noreply.github.com>

--- vcfa/config_test.go
+++ vcfa/config_test.go
@@ -1414,24 +1414,27 @@ var registeredPriorityTests = []priorityTest{}
 
 func runPriorityTestsOnce(t *testing.T) {
 	notExecuted := priorityTestsExecuted.CompareAndSwap(false, true)
-	if notExecuted {
-		fmt.Printf("# Running priority tests before shared vCenter and NSX Manager is created, so they do not collide later (can be skipped with '-vcfa-skip-priority-tests' flag)\n")
-		for _, test := range registeredPriorityTests {
-			fmt.Printf("# Running priority test '%s' as a subtest of '%s':\n", test.Name, t.Name())
-			t.Run(test.Name, test.Test)
-			printfTrace("## Storing test to executed test list '%s'\n", test.Name)
-			executedTests.Store(test.Name, !t.Failed())
-		}
-
-		// setup shared components for other tests
-		fmt.Printf("# Setting up shared vCenter and NSX Manager\n")
-		cleanup, err := setupVcAndNsx()
-		if err != nil {
-			fmt.Printf("error setting up shared VC and NSX: %s", err)
-		}
-		fmt.Printf("# Done setting up shared vCenter and NSX Manager\n")
-
-		priorityTestCleanupFunc = cleanup
-		fmt.Printf("# Continuing run of %s test after priority tests are now done\n", t.Name())
+	executed := !notExecuted
+	if executed {
+		return
 	}
+
+	fmt.Printf("# Running priority tests before shared vCenter and NSX Manager is created, so they do not collide later (can be skipped with '-vcfa-skip-priority-tests' flag)\n")
+	for _, test := range registeredPriorityTests {
+		fmt.Printf("# Running priority test '%s' as a subtest of '%s':\n", test.Name, t.Name())
+		t.Run(test.Name, test.Test)
+		printfTrace("## Storing test to executed test list '%s'\n", test.Name)
+		executedTests.Store(test.Name, !t.Failed())
+	}
+
+	// setup shared components for other tests
+	fmt.Printf("# Setting up shared vCenter and NSX Manager\n")
+	cleanup, err := setupVcAndNsx()
+	if err != nil {
+		fmt.Printf("error setting up shared VC and NSX: %s", err)
+	}
+	fmt.Printf("# Done setting up shared vCenter and NSX Manager\n")
+
+	priorityTestCleanupFunc = cleanup
+	fmt.Printf("# Continuing run of %s test after priority tests are now done\n", t.Name())
 }
